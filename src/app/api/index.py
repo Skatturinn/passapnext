1e2cb2b21d1863a25d314e1130d3d29c36c73f11
Elías <elias.ludviksson@gmail.com>
2024-08-22 20:49:57 +0000

    tst 2

--- src/app/api/index.py
+++ src/app/api/index.py
@@ -4,6 +4,9 @@ app = Flask(__name__)
 @app.route('/api/hello', methods=['GET'])
 def hello_world():
     return "Hello, World!"
+@app.route('/api', methods=['POST'])
+def hello_world():
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(port=5328)
